71a41732a2155962969af2b88f428e9ef873dd95
mitchgoertzen <mitchell.goertzen@ucalgary.ca>
2022-12-31 15:07:46 -0800

    refactor SearchModel

--- ai/SearchModel.java
+++ ai/SearchModel.java
@@ -5,25 +5,22 @@ import main.DraftSlot;
 import main.Env;
 
 public class SearchModel {
+
     public static ArrayList<Problem> Div(Problem prob, int max, int round, int id) {
-       // System.out.println("Div problem round: " + round);
-        //System.out.println("Env.currentRound : " + Env.getCurrentRound());
-       
-
-
 
         DraftSlot nextDraftSlot = prob.nextDraftSlot();
         ArrayList<String> availablePlayers = prob.getAvailablePlayers();
         ArrayList<Problem> subProblems = new ArrayList<>();
 
         prob.nextPick();
-        for(String player: availablePlayers.subList(0, 50)) {
+
+        int availablePlayerScope = 50;
+        for(String player: availablePlayers.subList(0, availablePlayerScope)) {
             Problem subProblem = new Problem(prob, max);
-            //if element is able to be added to the slot
             if (subProblem.draftPlayer(Env.AllPlayers.get(player), nextDraftSlot, round));
                 subProblems.add(subProblem);
             }
-
         return subProblems;
     }
+    
 }
