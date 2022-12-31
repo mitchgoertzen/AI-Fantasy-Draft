9e8fb2e2cb7abab8963aaa3bb72052d7e8e2b945
mitchgoertzen <mitchell.goertzen@ucalgary.ca>
2022-12-31 15:14:11 -0800

    refactor AIParticipant

--- main/AIParticipant.java
+++ main/AIParticipant.java
@@ -7,22 +7,15 @@ public class AIParticipant extends Participant {
 
     private final boolean DEBUG = false;
 
-    ArrayList<DraftSlot> draftSlots;
+    private ArrayList<DraftSlot> draftSlots;
 
-    int maxDraftSlots;
-    public int getMaxDraftSlots() {
-        return maxDraftSlots;
-    }
-    float rosterScore;
-    //total # of catgeory wins, determined each week 
-    int predictedSeasonScore;
-    //lw, rw, c, d, g
-    Integer[] positionCounts = new Integer[5];
+    private float rosterScore;
+    private float maxScore = -Float.MAX_VALUE;
 
+    private int maxDraftSlots;
+    private int predictedSeasonScore;
 
-    public Integer[] getPositionCounts() {
-        return positionCounts;
-    }
+    private Integer[] positionCounts = new Integer[5]; //lw, rw, c, d, g, f
 
     public AIParticipant(int id, boolean isHuman, int draftNumber, int maxDraftSlots) {
         super(id, isHuman, draftNumber);
@@ -34,61 +27,25 @@ public class AIParticipant extends Participant {
 		for(int i = 0; i < maxDraftSlots; i++){
             draftSlots.add(new DraftSlot(i));
         }
-
-
-    }
-
-    public String draftPlayer(){
-
-    	long StartTime = System.nanoTime();
-
-        ProblemState initialState = initializeProblemState();
-        initialState.setEval(rosterScore);
-
-    	//Run the search
-        ProblemState solution = runSearch(initialState);
-        String playerCode = solution.getMostRecentDraftSelection();
-
-        super.addPlayer(playerCode);
-        rosterScore += Env.PlayerScores.get(playerCode);
-        setMaxScore(rosterScore);
-    
-        int index = Env.getPositionIndex(Env.AllPlayers.get(playerCode).getPosition(), positionCounts);
-        if(DEBUG){        
-            System.out.println("player pos: " + Env.AllPlayers.get(playerCode).getPosition());
-            System.out.println("new count: " + (positionCounts[index] + 1));
-            System.out.println("limit: " + Env.getPositionLimits()[index]);
-        }
-
-        positionCounts[index]++;
-
-        System.out.println("score: " +  Env.PlayerScores.get(playerCode));
-        System.out.println("time elapsed: " + (System.nanoTime() - StartTime)/1000000000f + "s");
-        return playerCode;
     }
 
     @SuppressWarnings (value="unchecked")
     private ProblemState initializeProblemState() {
         
     	Problem initialProblem = new Problem(maxDraftSlots, super.getId());
-
         initialProblem.setAvailablePlayers((ArrayList<String>) DraftMenu.getAvailablePlayers().clone());
 
-        for(int i = 0; i < Env.participants.size(); i++){
+        int numParticipants = Env.participants.size();
+        for(int i = 0; i < numParticipants; i++){
             if(i != super.getId()){
                 initialProblem.setOpponentRoster(i, Env.participants.get(i).getRoster().getPlayers());
             }
         }
 
-
         initialProblem.setDraftSlots((ArrayList<DraftSlot>) draftSlots.clone());
 
         int i = 0;
-        // System.out.println(super.getRoster().hashCode());
-        // System.out.println(super.hashCode());
-        // System.out.println("Participant " + super.getId() + " has the roster of:");
         for(String s : super.getRoster().getPlayers()){
-         //   System.out.println("player: " + s);
             initialProblem.addDraftedPlayers(s, i++);
         }
 
@@ -110,22 +67,16 @@ public class AIParticipant extends Participant {
             System.out.println("leaves: " + control.getLeaves().size());
     	while ((!control.getLeaves().isEmpty())) {
 
-
-    		//Select the best leaf to work on
-            // if(DEBUG)
-            //     System.out.println("fleaf");
     		control.fleaf();
 
-    		//Decide what to do with the current leaf
-            // if(DEBUG)
-            //     System.out.println("ftrans");
     		control.ftrans();
     		currentState = control.getCurrentLeaf();
+
     		//If the solution is complete and it has a better eval value than the current best state,
     		//then update the best state to the current state
     		if (!(currentState == null)) {
-                // if(DEBUG)
-                //     System.out.println("current state not null");
+                if(DEBUG)
+                    System.out.println("current state not null");
     			bestState = currentState;
     		}
     	}
@@ -139,14 +90,50 @@ public class AIParticipant extends Participant {
         draftSlots.remove(0);
     	return bestState;
     }
+
+    public String draftPlayer(){
+
+    	long StartTime = System.nanoTime();
+
+        ProblemState initialState = initializeProblemState();
+        initialState.setEval(rosterScore);
+
+        ProblemState solution = runSearch(initialState);
+        String playerCode = solution.getMostRecentDraftSelection();
+
+        super.addPlayer(playerCode);
+        rosterScore += Env.PlayerScores.get(playerCode);
+        setMaxScore(rosterScore);
     
+        int index = Env.getPositionIndex(Env.AllPlayers.get(playerCode).getPosition(), positionCounts);
+        if(DEBUG){        
+            System.out.println("player pos: " + Env.AllPlayers.get(playerCode).getPosition());
+            System.out.println("new count: " + (positionCounts[index] + 1));
+            System.out.println("limit: " + Env.getPositionLimits()[index]);
+        }
+
+        positionCounts[index]++;
+
+        System.out.println("score: " +  Env.PlayerScores.get(playerCode));
+        System.out.println("time elapsed: " + (System.nanoTime() - StartTime)/1000000000f + "s");
+        return playerCode;
+    }
+
+   
     public void addEval(float eval){
         rosterScore += eval;
     }
-    private float maxScore = -Float.MAX_VALUE;
 
     public float getMaxScore() {
         return maxScore;
+    }
+
+    public int getMaxDraftSlots() {
+        return maxDraftSlots;
+    }
+    
+    public Integer[] getPositionCounts() {
+        return positionCounts;
     }
 
     public void setMaxScore(float maxScore) {
