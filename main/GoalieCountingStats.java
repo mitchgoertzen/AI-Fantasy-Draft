9df78659f70dca64800d996af387fc394849639c
mitchgoertzen <mitchell.goertzen@ucalgary.ca>
2023-01-05 12:50:20 -0800

    add statScore

--- main/GoalieCountingStats.java
+++ main/GoalieCountingStats.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 
 public class GoalieCountingStats {
    
+    public float[] getStatScore() {
+        return statScore;
+    }
     private int gamesStarted;
     private int goalsAgainst;
     private int losses;
@@ -11,7 +14,9 @@ public class GoalieCountingStats {
     private int shotsAgainst;
     private int shutouts;
     private int wins;
-    
+
+    private float[] statScore;
+
     private Integer[] stats; //GS,W,L,GA,SA,SV,SO
 
     public GoalieCountingStats(){
@@ -21,7 +26,6 @@ public class GoalieCountingStats {
 
     public GoalieCountingStats(String[] array){
         stats = new Integer[7];
-    
         gamesStarted = stats[0] = Integer.parseInt(array[3]);
         wins = stats[1] = Integer.parseInt(array[3]);
         losses = stats[2] = Integer.parseInt(array[3]);
@@ -29,6 +33,9 @@ public class GoalieCountingStats {
         shotsAgainst = stats[4] = Integer.parseInt(array[5]);
         saves =  stats[5] = Integer.parseInt(array[6]);
         shutouts = stats[6] = Integer.parseInt(array[7]);
+        
+        statScore = new float[7];
+        setStatScore();
     }
 
     public void addStats(Integer[] newStats, int gamesPlayed){
@@ -92,6 +99,12 @@ public class GoalieCountingStats {
 
     public void setShutouts(int shutouts) {
         this.shutouts = shutouts;
+    }
+
+    public void setStatScore(){
+        for(int i = 0; i < 7; i++){
+            statScore[i] = stats[i] * Env.getGoalieWeights(i);
+        }
     }
 
     public void setWins(int wins) {
